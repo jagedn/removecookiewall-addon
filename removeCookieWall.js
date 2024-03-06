761972a308f1c2ef9df4d18dff96f1a338e19551
Jorge Aguilera <jagedn@gmail.com>
2024-03-06 16:05:35 +0100

    add xataka

--- removeCookieWall.js
+++ removeCookieWall.js
@@ -1,30 +1,31 @@
 var readyStateCheckInterval;
 var counter = 0;
+
+function sanitizeBody() {
+    document.body.style.overflow = "unset"
+    document.body.classList.remove('sxnlzit')
+    document.body.classList.remove('didomi-popup-open')
+}
+
 readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
         counter++;
         const removeParent = ['div.pmConsentWall']; //elpais
         [...removeParent].forEach(s => {
-            console.log("search " + s)
             var divs = document.body.querySelectorAll(s);
             [...divs].forEach(element => {
-                console.log("remove " + s)
                 element.parentNode.remove()
-                document.body.style.overflow = "unset"
+                sanitizeBody()
                 clearInterval(readyStateCheckInterval);
             });
         });
-        const removeThis = ['div[data-nosnippet="data-nosnippet"]', '#mrf-popup']; //cadenaser
+        const removeThis = ['div[data-nosnippet="data-nosnippet"]', '#mrf-popup', '#didomi-popup'];
         [...removeThis].forEach(s => {
-            console.log("search " + s)
             var divs = document.body.querySelectorAll(s);
             [...divs].forEach(element => {
-                console.log(element)
                 setTimeout(() => {
-                    console.log(element);
+                    sanitizeBody()
                     element.remove()
-                    document.body.style.overflow = "unset"
-                    document.body.classList.remove('sxnlzit')
                     clearInterval(readyStateCheckInterval);
                 }, 1000);
             });
