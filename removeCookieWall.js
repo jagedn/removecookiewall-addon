c2ceb473c805dc88fdef5e5dd9dd8d7cdebc7c38
Jorge Aguilera <jagedn@gmail.com>
2024-03-07 08:01:52 +0100

    v0.9

--- removeCookieWall.js
+++ removeCookieWall.js
@@ -5,6 +5,7 @@ function sanitizeBody() {
     document.body.style.overflow = "unset"
     document.body.classList.remove('sxnlzit')
     document.body.classList.remove('didomi-popup-open')
+    document.body.parentNode.classList.remove('sp-message-open')
 }
 
 readyStateCheckInterval = setInterval(function() {
@@ -19,7 +20,7 @@ readyStateCheckInterval = setInterval(function() {
                 clearInterval(readyStateCheckInterval);
             });
         });
-        const removeThis = ['div[data-nosnippet="data-nosnippet"]', '#mrf-popup', '#didomi-popup'];
+        const removeThis = ['div[data-nosnippet="data-nosnippet"]', '#mrf-popup', '#didomi-popup', '[id^="sp_message_container_"]'];
         [...removeThis].forEach(s => {
             var divs = document.body.querySelectorAll(s);
             [...divs].forEach(element => {
