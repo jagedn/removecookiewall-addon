73a139ac271318f80c919e093a795c1b65b8988a
Jorge Aguilera <jagedn@gmail.com>
2024-12-14 08:47:00 +0100

    v1.0.0

--- removeCookieWall.js
+++ removeCookieWall.js
@@ -1,11 +1,15 @@
 var readyStateCheckInterval;
 var counter = 0;
+var config = null;
 
 function sanitizeBody() {
-    document.body.style.overflow = "unset"
+    document.body.style.overflow = 'unset'
     document.body.classList.remove('sxnlzit')
+    document.body.classList.remove('_y9ev9r')
     document.body.classList.remove('didomi-popup-open')
     document.body.parentNode.classList.remove('sp-message-open')
+
+    document.documentElement.className = '';
 }
 
 function removeMe(element) {
@@ -13,31 +17,42 @@ function removeMe(element) {
     sanitizeBody();
 }
 
-readyStateCheckInterval = setInterval(function() {
+readyStateCheckInterval = setInterval(async function() {
     if (document.readyState === "complete") {
+        if (!config) {
+            const data = await fetch("https://raw.githubusercontent.com/jagedn/removecookiewall-addon/refs/heads/main/config.json", {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            });
+            config = await data.json()
+        }
         counter++;
-        const removeParent = ['div.pmConsentWall']; //elpais
+        const removeParent = config.parents || [];
         [...removeParent].forEach(s => {
             var divs = document.body.querySelectorAll(s);
             [...divs].forEach(element => {
                 removeMe(element.parentNode);
             });
         });
-        const removeThis = [
-            'div[data-nosnippet="data-nosnippet"]',
-            '#mrf-popup',
-            '#didomi-popup',
-            '[id^="sp_message_container_"]',
-            '#cl-consent',
-            'dialog.cookie-policy'
-        ];
+        const removeThis = config.elements || [];
         [...removeThis].forEach(s => {
             var divs = document.body.querySelectorAll(s);
             [...divs].forEach(element => {
                 removeMe(element);
             });
         });
+
+        const removeTags = config.tags || [];
+        [...removeTags].forEach(s => {
+            var divs = document.body.getElementsByTagName(s);
+            [...divs].forEach(element => {
+                removeMe(element);
+            });
+        });
         if (counter > 30) {
+            sanitizeBody();
             clearInterval(readyStateCheckInterval);
         }
     }
